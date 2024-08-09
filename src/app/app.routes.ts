b2cdb5d29f2b8f31288157f39019e154199b5cc0
Arman Singh <118332394+armanzz@users.noreply.github.com>
2024-08-09 09:16:26 -0700

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { SplashScreenComponent } from './splash-screen/splash-screen.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: SplashScreenComponent }, // Show the splash screen first
+  { path: 'home', component: AppComponent },  
+  { path: '**', redirectTo: '', pathMatch: 'full' }   
+];
